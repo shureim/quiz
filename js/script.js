06964500efa8647ade0eaf936a9691e53565c3b1
tarick ali <tarickaliabdi@gmail.com>
2018-08-18 07:55:39 +0300

    created index.html css and js

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+var correct =function(one ,two ,three ,four ,five){
+   return one + two + three + four + five;
+}
+
+$(document).ready(function() {
+    $("form#que").submit(function(event) {
+
+    var question1 = parseInt($("input:radio[name=question1]:checked").val());
+    var question2 = parseInt($("input:radio[name=question2]:checked").val());
+    var question3 = parseInt($("input:radio[name=question3]:checked").val());
+    var question4 = parseInt($("input:radio[name=question4]:checked").val());
+    var question5 = parseInt($("input:radio[name=question5]:checked").val());
+    var finalCorrect =correct(question1,question2,question3,question4,question5);
+
+    $(".after-submit").text(finalCorrect);
+       event.preventDefault();
+       $("#after-submit").show();
+  });
+
+  $("p").click(function(){
+    $(".hide").toggle();
+    $(".show").toggle();
+  });
+
+});
